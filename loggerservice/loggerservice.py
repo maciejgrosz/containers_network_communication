037b9e958f0f109a52ceebf57f5bd1568ddb7940
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-08-25 18:19:31 +0200

    first init

---
+++ loggerservice/loggerservice.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from datetime import datetime
+import requests
+from flask import request
+
+app = Flask(__name__)
+logs = []
+@app.route("/usage.log", methods=['GET', 'POST'])
+def home():
+    now = datetime.now()
+    r = requests.get('http://172.18.0.2:5000')
+    logs.append(r.text)
+    
+#    y = requests.args.get()
+    return '\n'.join(logs)
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ timerservice/timerservice.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+url="http://172.18.0.3:5000"
+log_url="http://172.18.0.3:5000"
+test = {'somekey': 'somevalue'}
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    now = datetime.now()
+    message = f'Today is: {now.strftime("%m/%d/%Y, %H:%M:%S")}'
+    test = {'somekey': 'somevalue'}
+    # r = requests.get(log_url)
+    r = requests.post(log_url,  data = message )
+    return f'{message}'
+
+if __name__ == "__main__":
+    app.run()
