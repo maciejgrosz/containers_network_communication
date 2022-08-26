ee123c65fdf008f290172e1357f5632c6ec7e7e4
Maciej Groszyk <maciej.groszyk8@gmail.com>
2022-08-26 08:52:50 +0200

    reformatting code and delete useless imports

--- loggerservice/loggerservice.py
+++ loggerservice/loggerservice.py
@@ -6,9 +6,10 @@ from flask import request
 app = Flask(__name__)
 logs = []
 
+
 @app.route("/list", methods=["POST"])
 def list():
-    r = request.data.decode('utf-8')
+    r = request.data.decode("utf-8")
     logs.append(r)
     return jsonify(success=True)
 
@@ -16,9 +17,6 @@ def list():
 @app.route("/usage.log")
 def home():
     return "<br>".join(logs)
-
-
-
 
 
 if __name__ == "__main__":
